8191948cc614f6da7eecf9290235df0dfd1cba13
Chris Aitken <caitken86@gmail.com>
2025-02-03 09:14:01 +0000

    stripped out authentication

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
-import { authRouter } from "./auth/auth.routes";
-import { usersRouter } from "./users/users.routes";
 
 const apiRouter = express.Router();
 
 apiRouter.get("/", async (req, res) => { res.json({ hello: "world" }) });
 
-apiRouter.use("/auth", authRouter);
-apiRouter.use("/users", usersRouter);
+apiRouter.post("/", (req, res) => {
+    const { name, location } = req.body;
+    res.status(200).send({
+        message: `YOUR KEYS WERE ${name},${location}`
+    })
+})
+
+apiRouter.get("/setup", () => {
+
+})
 
 export { apiRouter };
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import express from "express";
 import { apiRouter } from "./api";
-import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser());
 app.use(cors());
 
 app.use("/api", apiRouter);
 
-app.listen(3000, () => console.log("listening on http://localhost:3000"));
+app.listen(3000, () => console.log("http://localhost:3000/api"));
